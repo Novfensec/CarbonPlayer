c49679d61ea8910e05978aeeb9a8667cd7fefb1c
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-05-02 18:14:54 +0530

    Update

--- android_utils.py
+++ android_utils.py
@@ -1,9 +1,11 @@
 from kivy.utils import platform
+from kivy.app import App
 
 if platform == "android":
     from jnius import autoclass
     from android import mActivity
     from android.runnable import run_on_ui_thread
+    from carbonkivy.utils import parse_color
 
     ActivityInfo = autoclass("android.content.pm.ActivityInfo")
     View = autoclass("android.view.View")
@@ -12,8 +14,10 @@ if platform == "android":
     BuildVERSION = autoclass("android.os.Build$VERSION")
     ContentView = mActivity.getWindow().findViewById(autoclass("android.R$id").content)
 
+
 def maximize_video():
     if platform == "android":
+
         @run_on_ui_thread
         def _maximize():
             window = mActivity.getWindow()
@@ -39,35 +43,19 @@ def maximize_video():
                 window.setAttributes(attributes)
 
             decor_view.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             )
 
         _maximize()
 
+
 def minimize_video():
     if platform == "android":
-        listener_ref = None
-        app_bg_color = Color.BLACK
-        
-        try:
-            from carbonkivy.app import CarbonApp
-            from carbonkivy.utils import parse_color
-            
-            app_bg_color = parse_color(CarbonApp.get_running_app().background)
-        except Exception:
-            pass
-
-        if Build.VERSION.SDK_INT >= 35:
-            try:
-                from carbonkivy.utils import _global_listener
-                listener_ref = _global_listener
-            except ImportError:
-                pass
 
         @run_on_ui_thread
         def _minimize():
@@ -94,7 +82,7 @@ def minimize_video():
                 except ImportError:
                     pass
             else:
-                window.setNavigationBarColor(app_bg_color)
-                window.setStatusBarColor(app_bg_color)
+                window.setNavigationBarColor(parse_color(App.get_running_app().background))
+                window.setStatusBarColor(parse_color(App.get_running_app().background))
 
         _minimize()
